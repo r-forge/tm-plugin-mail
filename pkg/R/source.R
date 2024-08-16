4afe94ae28c189ace735e629aff5ddb563be87bf
khornik (Kurt Hornik) <Kurt.Hornik@wu.ac.at>
2024-08-16 16:32:15 +0000

    Do raw message as bytes. Add support for mbox/mboxo/mboxrd, and improve detection of 'From ' lines.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tm-plugin-mail@31 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/source.R
+++ pkg/R/source.R
@@ -1,9 +1,14 @@
 MBoxSource <-
-function(mbox, encoding = "")
+function(mbox, format = "mbox", delim = NULL)
 {
-    SimpleSource(encoding = encoding, file = NULL, length = 0,
-                 reader = readMail, mbox = mbox, msgLines = 0,
-                 msgOffsets = 0, class = "MBoxSource")
+    format <- match.arg(format, c("mbox", "mboxo", "mboxrd"))
+    if(is.null(delim))
+        delim <- if(format == "mbox") delim_mbox else "^From "
+    
+    SimpleSource(encoding = "bytes", reader = readMail,
+                 mbox = mbox, format = format, delim = delim,
+                 file = NULL, msgOffsets = 0, msgLengths = 0, 
+                 class = "MBoxSource")
 }
 
 close.MBoxSource <-
@@ -15,7 +20,7 @@ function(con, ...)
         if(!is.null(x$compress))
             unlink(x$file)
         x$file <- NULL
-        x$length <- x$msgLines <- x$msgOffsets <- 0
+        x$length <- x$msgOffsets <- x$msgLengths <- 0
     }
     x
 }
@@ -24,13 +29,13 @@ getElem.MBoxSource <-
 function(x)
 {
     stopifnot(!is.null(x$file))
-
     seek(x$file, x$msgOffsets[x$position])
-    list(content = iconv(readLines(x$file,
-                                   x$msgLines[x$position],
-                                   warn = FALSE),
-                         x$encoding, "UTF-8", "byte"),
-         uri = x$mbox)
+    lines <- read_bytes(x$file, x$msgLengths[x$position])
+    if(x$format == "mboxo")
+        lines <- sub("^>From ", "From ", lines)
+    else if(x$format == "mboxrd")
+        lines <- sub("^>(>*From )", "\\1", lines)
+    list(content = lines, uri = x$mbox)
 }
 
 open.MBoxSource <-
@@ -50,7 +55,7 @@ function(con, ...)
          else
              NULL
     if(!is.null(z)) {
-        lines <- readLines(z, warn = FALSE)
+        lines <- read_bytes(z)
         close(z)
         ## Text connections are not seekable, so we really need a
         ## tempfile ...
@@ -61,19 +66,54 @@ function(con, ...)
     } else
         x$file <- file(x$mbox)
     open(x$file)
-    message.nr <- 0L
-    offsets <- numeric()
-    lines <- integer()
-    while (length(line <- readLines(x$file, 1L, warn = FALSE)) == 1L) {
-        if (startsWith(line, "From ")) {
-            message.nr <- message.nr + 1L
-            offsets[message.nr] <- seek(x$file)
-            lines[message.nr] <- 0L
-        } else
-            lines[message.nr] <- lines[message.nr] + 1L
+    
+    .offsets <- numeric()
+    .lengths <- integer()
+    blank <- TRUE
+    num <- 0L
+    len <- 0L
+    while(length(line <- read_bytes(x$file, 1L)) == 1L) {
+        if(blank && grepl(x$delim, line)) {
+            .lengths[num] <- len
+            num <- num + 1L
+            len <- 0L
+            .offsets[num] <- seek(x$file)
+            blank <- FALSE
+        } else {
+            len <- len + 1L
+            blank <- !nzchar(line)
+        }
     }
-    x$length <- length(lines)
-    x$msgLines <- lines
-    x$msgOffsets <- offsets
+    .lengths[num] <- len
+    x$length <- length(.lengths)
+    x$msgLengths <- .lengths
+    x$msgOffsets <- .offsets
     x
 }
+
+read_bytes <- function(con, n = -1L)
+    readLines(con, n, warn = FALSE, encoding = "bytes")
+
+delim_mbox <-
+    paste0("^From ",
+           ".* ",
+           sprintf("(%s) ",
+                   paste(c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri",
+                           "Sat"),
+                         collapse = "|")),
+           sprintf("(%s) ",
+                   paste(c("Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                           "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"), 
+                         collapse = "|")),
+           "[ 0-3][0-9] ",
+           "[0-2][0-9]:[0-5][0-9]:[0-6][0-9] ",
+           sprintf("(%s)? *",
+                   paste(c("[A-Z]?[A-Z]?[A-Z][A-Z]( DST)?",
+                           "[+-]?[0-9][0-9][0-9][0-9]",
+                           ""),
+                         collapse = "|")),
+           "[ 0-9][ 0-9][ 0-9][0-9]",
+           ".*$")
+    
+
+
