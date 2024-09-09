d25187ffa62ac9146e80679f80e7d7eb85300d8f
khornik (Kurt Hornik) <Kurt.Hornik@wu.ac.at>
2024-09-09 10:01:09 +0000

    Updated.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/tm-plugin-mail@41 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Work/qpstuff.R
+++ pkg/Work/qpstuff.R
@@ -1,11 +1,3 @@
-Sys.setenv("RETICULATE_AUTOCREATE_PACKAGE_VENV" = "false")
-.py_quopri <- reticulate::import("quopri")
-.py_quopri$encodestring(charToRaw("München"))
-.py_quopri$encodestring(charToRaw(iconv("München", to = "latin1")))
-
-.py_quopri$decodestring("sur la route =C3=A0 suivre les voil=C3=A0 bient=C3=B4t qui te d=C3=A9gradent")
-.py_quopri$decodestring("stern=2Ede_-_t=C3=A4glich")$decode("UTF-8")
-
 qpdecode <- function(x, header = FALSE) {
     x <- gsub("=\n", "", x)
     if(header)
@@ -37,5 +29,39 @@ qpdecode <- function(x, header = FALSE) {
            USE.NAMES = FALSE)
 }
 
+qpdecode_mime_header <- function(s) {
+    s <- gsub("[[:space:]]+", " ", s)
+    p <- "=[?]([^?]+)[?]Q[?]([^?]+)[?]=[[:space:]]?"
+    m <- gregexpr(p, s)
+    regmatches(s, m) <-
+        lapply(regmatches(s, m),
+               function(e) qpdecode(sub(p, "\\2", e)))
+    s
+}
+
+if(FALSE) {
+    s <- "X-CRAN-Issue-Info: =?us-ascii?Q?2024-09-09%5F09:40:33%5Fhornik:ERROR%5Ffrom%5Fupgrade%5Fsf:Q?=
+    =?us-ascii?Q?UERIED:2024-09-09:hornik?="
+
+    s <- "X-CRAN-Issue-Info: =?us-ascii?Q?2024-09-09=5F09:20:53=5Fhornik:ERROR=5Ffrom=5Fupgrade=5Fmsm?=
+    =?us-ascii?Q?:QUERIED:2024-09-09:hornik:BEFORE:2024-09-23?="
+
+    s <- "X-CRAN-Issue-Info: =?us-ascii?Q?2024-09-09=5F09:20:53=5Fhornik:ERROR=5Ffrom=5Fupgrade=5Fmsm?=
+    =?us-ascii?Q?:QUERIED:2024-09-09:hornik:BEFORE:2024-09-23?="
+    qpdecode_mime_header(s)
+}
+
+if(FALSE) {
 qpdecode("sur la route =C3=A0 suivre les voil=C3=A0 bient=C3=B4t qui te d=C3=A9gradent")
 qpdecode("stern=2Ede_-_t=C3=A4glich", header = TRUE)
+}
+
+if(FALSE) {
+Sys.setenv("RETICULATE_AUTOCREATE_PACKAGE_VENV" = "false")
+.py_quopri <- reticulate::import("quopri")
+.py_quopri$encodestring(charToRaw("München"))
+.py_quopri$encodestring(charToRaw(iconv("München", to = "latin1")))
+
+.py_quopri$decodestring("sur la route =C3=A0 suivre les voil=C3=A0 bient=C3=B4t qui te d=C3=A9gradent")
+.py_quopri$decodestring("stern=2Ede_-_t=C3=A4glich")$decode("UTF-8")
+}    
